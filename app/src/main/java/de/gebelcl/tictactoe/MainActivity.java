612e14a3d2a179d4b07a2bf0a823be482b2de757
thekrazykat <60964813+thekrazykat@users.noreply.github.com>
2020-11-30 17:04:08 +0100

    TicTacToe completed

--- app/src/main/java/de/gebelcl/tictactoe/MainActivity.java
+++ app/src/main/java/de/gebelcl/tictactoe/MainActivity.java
@@ -2,12 +2,141 @@ package de.gebelcl.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayout;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button playAgainButton;
+    TextView winnerTextView;
+    GridLayout gridLayout;
+    boolean gewonnen = false;
+
+    // 0 = player 1 clicked that field
+    // 1 = player 2 clicked that field
+    // 2 = unclicked field
+    private int[] gameState = {
+            2, 2, 2,
+            2, 2, 2,
+            2, 2, 2
+    };
+
+    private int[][] winPositions = {
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+            {0, 4, 8}, {2, 4, 6}
+    };
+
+    private int activePlayer = 0;
+
+    private boolean gameIsActive = true;
+
+    public void onViewClicked(View view) {
+        ImageView imageView = (ImageView) view;
+
+        int clickedImageViewNumber = Integer.parseInt(imageView.getTag().toString());
+
+        if (gameIsActive && gameState[clickedImageViewNumber] == 2) {
+            gameState[clickedImageViewNumber] = activePlayer;
+            if (activePlayer == 0) {
+                imageView.setImageResource(R.drawable.ring);
+                activePlayer = 1;
+            } else {
+                imageView.setImageResource(R.drawable.cross);
+                activePlayer = 0;
+            }
+            imageView.animate().alpha(1).setDuration(1000);
+
+            for (int[] winPosition : winPositions) {
+                if (gameState[winPosition[0]] == gameState[winPosition[1]] && gameState[winPosition[1]] == gameState[winPosition[2]] && gameState[winPosition[0]] != 2) {
+                    gameIsActive = false;
+                    // deactivate clickability of imageViews
+                    clickBehaviorOfImageViews(false);
+                    String winner = "";
+                    if (activePlayer == 1) {
+                        gewonnen = true;
+                        winner = "Ring";
+                        activePlayer = 0; // change active player so that the winner will begin in the next game
+                    } else {
+                        gewonnen = true;
+                        winner = "Kreuz";
+                        activePlayer = 1; // change active player so that the winner will begin in the next game
+                    }
+
+                    winnerTextView.setText(winner + " hat gewonnen!");
+                    playAgainButton.setVisibility(View.VISIBLE);
+                    winnerTextView.setVisibility(View.VISIBLE);
+
+                    stressWinnersImageView(gameState[winPosition[0]], gameState[winPosition[1]], gameState[winPosition[2]]);
+                    Log.i("1", String.valueOf(gameState[winPosition[0]]));
+                    Log.i("2", String.valueOf(gameState[winPosition[1]]));
+                    Log.i("3", String.valueOf(gameState[winPosition[2]]));
+
+                } else {
+                    gameIsActive = false;
+
+                    for (int imageViewState: gameState) {
+                        if (imageViewState == 2) {
+                            gameIsActive = true;
+                        }
+                    }
+
+                    if (!gameIsActive) {
+                        winnerTextView.setText("Unentschieden!");
+                        playAgainButton.setVisibility(View.VISIBLE);
+                        winnerTextView.setVisibility(View.VISIBLE);
+                        // Anklickbarkeit der ImageViews deaktivieren
+                        clickBehaviorOfImageViews(false);
+                    }
+                }
+            }
+        }
+    }
+
+    public void playAgain(View view) {
+        playAgainButton.setVisibility(View.INVISIBLE);
+        winnerTextView.setVisibility(View.INVISIBLE);
+        gewonnen = false;
+        // Anklickbarkeit der ImageViews deaktivieren
+        clickBehaviorOfImageViews(true);
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            ImageView imageView = (ImageView) gridLayout.getChildAt(i);
+            imageView.setImageDrawable(null);
+        }
+        for (int i = 0; i < gameState.length; i++) {
+            gameState[i] = 2;
+        }
+        gameIsActive = true;
+    }
+
+    // if parameter is true -> imageViews are clickable
+    // if parameter is false -> imageViews are not clickable
+    protected void clickBehaviorOfImageViews(boolean clickable) {
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            ImageView iv = (ImageView) gridLayout.getChildAt(i);
+            iv.setClickable(clickable);
+        }
+    }
+
+    protected void stressWinnersImageView(int image1, int image2, int image3) {
+        for (int i = 0; i < 9; i++) {
+            ImageView iv = (ImageView) gridLayout.getChildAt(i);
+            if (i==image1 || i == image2 || i == image3) {
+                iv.setAlpha(1.0f);
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playAgainButton = (Button) findViewById(R.id.playAgainButton);
+        winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+        gridLayout = (GridLayout) findViewById((R.id.gridlayout));
     }
 }
